8b3abeb68e10cc4f6d9834379b7f0906be440894
SHOTAiiMURA <hn.j.shota027503@gamil.com>
2024-06-07 21:05:15 +0900

    switch

--- grammar.java
+++ grammar.java
@@ -29,7 +29,25 @@ public class grammar {
             System.out.println(j);
         }
         int[][] myNumbers = {{1,2,3},{4,5,6},{7,8,9}};
-
+// Switch
+        int day = 5;
+        switch(day){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+        }
 
     }
 }
